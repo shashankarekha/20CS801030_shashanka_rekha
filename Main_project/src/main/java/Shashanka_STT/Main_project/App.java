1472c960c5874628b0897603e93cfdb1ab44253c
shashankarekha <107735166+shashankarekha@users.noreply.github.com>
2022-06-18 11:47:36 +0530

    Latest code

---
+++ Main_project/src/main/java/Shashanka_STT/Main_project/App.java
@@ -0,0 +1,13 @@
+package Shashanka_STT.Main_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
